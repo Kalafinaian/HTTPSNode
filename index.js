5e37ef25f9c33a969a3daf9f3041edd6234c3cc5
chenganglist <1464125055@qq.com>
2016-04-01 22:44:02 +0800

    重命名用户管理模块

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ var server = require("./server"); //https服务模块
 
 var router = require("./router"); //路由配置模块
 
-var requestHandlers = require("./requestHandlers");  //请求处理模块
+var userInfoHandlers = require("./userInfoHandlers");  //用户信息请求处理模块
 
 
 //---------------------开始--对象handle：存储处理函数--开始--------------------//
 var handle = {};
-handle["/v0/"] = requestHandlers.login;
-handle["/v0/login"] = requestHandlers.login;
-handle["/v0/addUser"] = requestHandlers.addUser;
-handle["/v0/selectUser"] = requestHandlers.selectUser;
-handle["/v0/deleteUser"] = requestHandlers.deleteUser;
-handle["/v0/updateUser"] = requestHandlers.updateUser;
+handle["/v0/"] = userInfoHandlers.login;
+handle["/v0/login"] = userInfoHandlers.login;
+handle["/v0/addUser"] = userInfoHandlers.addUser;
+handle["/v0/selectUser"] = userInfoHandlers.selectUser;
+handle["/v0/deleteUser"] = userInfoHandlers.deleteUser;
+handle["/v0/updateUser"] = userInfoHandlers.updateUser;
 //---------------------结束--对象handle：存储处理函数--结束--------------------//
 
 
