313b9f7f1830c2d6ecd28342aa02a98775442340
chenganglist <1464125055@qq.com>
2016-06-26 11:37:51 +0800

    文件上传服务

---
+++ upload/uploadFile.js
@@ -0,0 +1,70 @@
+var httpsModule = require("https");
+var fs = require('fs');
+var url = require("url");
+var formidable = require('formidable');
+var util = require('util');
+var uuid = require('node-uuid');
+
+//---------------------开始--https启动监听，路由分配函数--开始--------------------//
+function onRequest(request,response)
+{
+	var postData = "";
+	var pathname = url.parse(request.url).pathname;
+	console.log("Request for " + pathname + " started.");
+
+	if( pathname == '/uploadFile' && request.method.toLowerCase() == 'post' )
+	{
+		console.log("start upload");
+
+		//创建上传表单
+		var form = new formidable.IncomingForm();
+		//设置编辑
+		form.encoding = 'utf-8';
+		//设置上传目录
+		form.uploadDir = "./";
+		form.keepExtensions = true;
+		//文件大小
+		form.maxFieldsSize = 10 * 1024 * 1024;
+		form.parse(request, function (err, fields, files) {
+			if(err) {
+				response.send(err);
+				return;
+			}
+			//console.log(fields);
+
+			console.log(files);
+
+			var extName = /\.[^\.]+/.exec(files.name);
+			var ext = Array.isArray(extName)
+				? extName[0]
+				: '';
+			//重命名，以防文件重复
+			var avatarName = uuid() + ext;
+			//移动的文件目录
+			var newPath = form.uploadDir + avatarName;
+			fs.renameSync(files.path, newPath);
+
+			response.send('success');
+		});
+
+		return;
+	}
+
+	
+}
+
+var options = {
+     key: fs.readFileSync('/etc/nginx/server.key'),
+     cert: fs.readFileSync('/etc/nginx/server.crt')
+};
+
+var https = httpsModule.createServer(options, onRequest);
+
+https.listen(8080, function(err){  
+    console.log("https listening on port: 8080");
+});
+console.log("server has started");
+
+
+//---------------------结束--https启动监听，路由分配函数--结束--------------------//
+
